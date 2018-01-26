89985c743b6cb7c720ae26221cbf9be9a776a7a8
Arthur Lui <luiarthur@gmail.com>
2018-01-25 23:29:40 -0800

    yes

--- genData.R
+++ genData.R
@@ -3,3 +3,5 @@ X = matrix(rnorm(50000 * 32), 50000, 32)
 
 y = rep(as.list(X), 10)
 save(y, file='data/dat.RData')
+
+write.table(X, file='data/dat.txt', quote=FALSE, col.names=FALSE, row.names=FALSE)
