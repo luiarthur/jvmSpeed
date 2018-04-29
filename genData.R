c33dd0db6c73cdd47b868a529aa7221bc62b2bd0
Arthur Lui <luiarthur@gmail.com>
2018-04-28 23:09:17 -0700

    yes

--- genData.R
+++ genData.R
@@ -1,7 +1,13 @@
-X = matrix(rnorm(50000 * 32), 50000, 32)
+#X = matrix(rnorm(50000 * 32), 50000, 32)
+#X = matrix(sample(1:10, 50000 * 32, replace=TRUE), 50000, 32)
 #save(X, file='data/dat.RData')
 
-y = rep(as.list(X), 10)
-save(y, file='data/dat.RData')
+### IntegerMatrix is cheaper by an order of magnitude
+#y = lapply(as.list(1:10), function(i)
+#           matrix(sample(1:10, 50000 * 32, replace=TRUE), 50000, 32))
 
-write.table(X, file='data/dat.txt', quote=FALSE, col.names=FALSE, row.names=FALSE)
+y = lapply(as.list(1:10), function(i)
+           matrix(rnorm(50000 * 32), 50000, 32))
+saveRDS(y, file='data/dat.rds')
+
+#write.table(X, file='data/dat.txt', quote=FALSE, col.names=FALSE, row.names=FALSE)
